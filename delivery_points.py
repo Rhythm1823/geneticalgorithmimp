cdb72e3946f564d6f25ee327933a0399b53bd3fe
Rhythm Pantha <83492950+Rhythm1823@users.noreply.github.com>
2024-08-06 14:08:57 +0545

    delivery_points.py

---
+++ delivery_points.py
@@ -0,0 +1,33 @@
+# delivery_points.py
+
+# Define delivery points (latitude, longitude)
+DELIVERY_POINTS = [
+    (27.7172, 85.3240),  # Kathmandu
+    (27.6710, 85.3240),  # Bhaktapur
+    (27.6622, 85.4374),  # Patan
+    (27.6943, 85.3285),  # Lalitpur
+    (27.6830, 85.3192),  # Kirtipur
+    (27.6923, 85.3324),  # Madhyapur Thimi
+    (27.6884, 85.3423),  # Nagarjun
+    (27.7223, 85.3240),  # Gokarneshwor
+    (27.7182, 85.3152),  # Budhanilkantha
+    (27.7046, 85.3234),  # Jorpati
+    (27.7172, 85.3092),  # Teku
+    (27.7110, 85.3198),  # Chabahil
+    (27.6928, 85.3273),  # Sanepa
+    (27.7070, 85.3236),  # Gaushala
+    (27.7040, 85.3162),  # Patan Durbar Square
+    (27.6901, 85.3215),  # Boudhanath Stupa
+    (27.6880, 85.3370),  # Swayambhunath Stupa
+    (27.6898, 85.3245),  # Pashupatinath Temple
+    (27.7172, 85.3242),  # Hanuman Dhoka
+    (27.6948, 85.3202)   # Balaju
+]
+
+# Labels for the delivery points
+LABELS = [
+    "Kathmandu", "Bhaktapur", "Patan", "Lalitpur", "Kirtipur", "Madhyapur Thimi",
+    "Nagarjun", "Gokarneshwor", "Budhanilkantha", "Jorpati", "Teku", "Chabahil",
+    "Sanepa", "Gaushala", "Patan Durbar Square", "Boudhanath Stupa", 
+    "Swayambhunath Stupa", "Pashupatinath Temple", "Hanuman Dhoka", "Balaju"
+]
